104cebffa3c62bf7df34b10d381b7d8e85cdc72c
Moshe Dego <moshedego@gmail.com>
2020-04-27 20:15:15 +0300

    Updates

---
+++ precache-manifest.3fb55aebd8027d4b1ecd22cd9991e0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011db52875eefb2a4d701deda121a3d6",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "012c4354d93ee7810209",
+    "url": "/robofriends-redux/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "75ec6d1b17534fe1bd90",
+    "url": "/robofriends-redux/static/css/main.c554d052.chunk.css"
+  },
+  {
+    "revision": "012c4354d93ee7810209",
+    "url": "/robofriends-redux/static/js/2.d53453c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-redux/static/js/2.d53453c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ec6d1b17534fe1bd90",
+    "url": "/robofriends-redux/static/js/main.fd260e61.chunk.js"
+  },
+  {
+    "revision": "031c7a520b6fbbb7b5c4",
+    "url": "/robofriends-redux/static/js/runtime-main.7719f2e8.js"
+  }
+]);
